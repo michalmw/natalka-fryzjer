d5d88719e1f234adaf5c0ab00b184ee2a36260a2
michalmw <michal@surprise.design>
2020-04-13 15:34:35 +0200

    Employee Module

---
+++ src/app/employee/employee.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { ViewComponent } from './view/view.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent
+  },
+  {
+    path: 'add',
+    component: AddComponent
+  },
+  {
+    path: 'edit',
+    component: EditComponent
+  },
+  {
+    path: 'view',
+    component: ViewComponent
+  }
+];
+
+
+@NgModule({
+  declarations: [ListComponent, AddComponent, EditComponent, ViewComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class EmployeeModule { }
